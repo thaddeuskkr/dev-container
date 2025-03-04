4c169ec7eb45502b8857a3cb56c75265ebc646f8
thaddeus kuah <tk@tkkr.dev>
2025-03-04 18:19:30 +0800

    feat: make the commands run simul

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,4 @@ cp -R /volumes/.gnupg/* /home/ubuntu/.gnupg
 cp /volumes/.gitconfig /home/ubuntu
 chown -R ubuntu:ubuntu /home/ubuntu/.gnupg
 chown -R ubuntu:ubuntu /home/ubuntu/.gitconfig
-/sbin/service sshd start
-sudo -u ubuntu bash -c 'code tunnel --accept-server-license-terms --server-data-dir /data/server --extensions-dir /data/extensions --cli-data-dir /data/cli'
+/usr/sbin/sshd -D & sudo -u ubuntu bash -c 'code tunnel --accept-server-license-terms --server-data-dir /data/server --extensions-dir /data/extensions --cli-data-dir /data/cli'
