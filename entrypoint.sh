a7279a39b3bac97795a58f77fa5ff089286d9386
thaddeus kuah <tk@tkkr.dev>
2025-03-04 18:43:29 +0800

    feat: use if

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+if [ -z "$PASSWORD" ]; then
+  PASSWORD=$(head /dev/urandom | tr -dc 'A-Za-z0-9' | head -c 16)
+  export PASSWORD
+fi
 echo "root:$PASSWORD" | chpasswd
 echo "ubuntu:$PASSWORD" | chpasswd
 mkdir -p /home/ubuntu/.ssh
 mkdir -p /home/ubuntu/.gnupg
-echo "$KEYS\n" >> /home/ubuntu/.ssh/authorized_keys
+if [ -n "$KEYS" ]; then
+  echo "$KEYS\n" >> /home/ubuntu/.ssh/authorized_keys
+fi
 cp -R /volumes/.gnupg/* /home/ubuntu/.gnupg
 cp /volumes/.gitconfig /home/ubuntu
 chown -R ubuntu:ubuntu /home/ubuntu/.gnupg
 chown -R ubuntu:ubuntu /home/ubuntu/.gitconfig
 chown -R ubuntu:ubuntu /workspaces
 chown -R ubuntu:ubuntu /data
+if [ -z "$DOCKER_GROUP" ]; then
+  groupadd docker_host
+else
+  groupadd -g $DOCKER_GROUP docker_host
+fi
+usermod -aG docker_host ubuntu
 /usr/sbin/sshd -D & sudo -u ubuntu bash -c 'code tunnel --accept-server-license-terms --server-data-dir /data/server --extensions-dir /data/extensions --cli-data-dir /data/cli'
