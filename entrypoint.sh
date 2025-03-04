93577b216007da363b37853b41871036eeed544d
thaddeus kuah <tk@tkkr.dev>
2025-03-04 17:44:48 +0800

    feat: new entrypoint script

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "root:$PASSWORD" | chpasswd
+echo "ubuntu:$PASSWORD" | chpasswd
+mkdir -p /home/ubuntu/.ssh
+mkdir -p /home/ubuntu/.gnupg
+echo "$KEYS\n" >> /home/ubuntu/.ssh/authorized_keys
+cp -R /volumes/.gnupg/* /home/ubuntu/.gnupg
+cp /volumes/.gitconfig /home/ubuntu
+chown -R ubuntu:ubuntu /home/ubuntu/.gnupg
+chown -R ubuntu:ubuntu /home/ubuntu/.gitconfig
+sudo systemctl enable ssh.service
+sudo systemctl start ssh.service
+sudo -u ubuntu bash -c 'code tunnel --accept-server-license-terms --server-data-dir /data/server --extensions-dir /data/extensions --cli-data-dir /data/cli'
